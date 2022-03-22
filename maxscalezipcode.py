77730c700c962027aef40e24494eb3e7b662e212
mjtomlinson <mjtomlinson@student.rtc.edu>
2022-03-22 03:10:39 -0700

    Update maxscalezipcode.py

--- maxscalezipcode.py
+++ maxscalezipcode.py
@@ -10,16 +10,16 @@
 
 import pymysql
 
-db = pymysql.connect(host="172.18.158.93", port=8080, user+"maxuser, passwd+"maxpwd"
+db = pymysql.connect(host="172.18.158.93", port=8080, user = "maxuser, passwd = "maxpwd"
 cursor = db.cursor()
 
-print('The last 10 rows of zipcodes_one are:')
+print ('The last 10 rows of zipcodes_one are:')
 cursor.execute("SELECT * FROM zipcodes_one.zipcondes_one LIMIT 9990.10;"
 results = cursor.fetchall()
 for result in results:
 	print (result)
 
-print('The first 10 rows of zipcodes_two are:')
+print ('The first 10 rows of zipcodes_two are:')
 cursor.execute("SELECT * FROM zipcodes_two.zipcodes_two LIMIT 10")
 results = cursor.fetchall()
 for result in results:
@@ -30,7 +30,7 @@ cursor = db.cursor()
 cursor.execute ("Select Zipcode FROM zipcodes_one.zipcodes_one ORDER BY Zipcode DESC LIMIT 1;"
 results = cursor.fetchall()
 for result in results:
-	print(result)
+	print (result)
 
 print ('The smallest zipcode number in zipcodes_two is:')
 cursor = db.cursor()
